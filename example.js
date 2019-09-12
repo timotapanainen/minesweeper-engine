ef607772d05d377829a03408b66e2d9393c7a3c5
Timo Tapanainen <timo.tapanainen@gmail.com>
2019-09-12 12:39:39 +0300

    promisefied example

--- example.js
+++ example.js
@@ -1,38 +1,73 @@
-
 const {Minesweeper, Difficulty, SquareState} = require('.');
-let ms;
 
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-console.log('Welcome to Minesweeper!');
-
-askDifficulty((difficultyName) =>  {
-    ms = new Minesweeper(Difficulty.valueOf(difficultyName));
-    askCoordinateToReveal(function revealHandler(x, y) {
-        ms.revealSquareAt({x: parseInt(x), y: parseInt(y)});
-        if (ms.isEnded()) {
-            printMinefield();
-            process.exit(0);
-        } else
-            askCoordinateToReveal(revealHandler);
-    });
-});
-
-function askDifficulty(callback) {
-    readline.question(`Select one of the following difficulty levels (beginner, intermediate, expert):`, callback);
+function playGame() {
+    console.log('Welcome to Minesweeper!');
+    askDifficultyUntilOk()
+        .then(difficulty => new Minesweeper(Difficulty.valueOf(difficulty)))
+        .then((game) => playRound(game))
+        .catch(err => console.log(err));
 }
 
-function printMinefield() {
-    console.log(ms.toString());
-}
-
-function askCoordinateToReveal(callback) {
-    printMinefield();
-    readline.question(`Specify square to reveal (x,y):`, (xy) => {
-        let [x,y] = xy.split(',');
-        callback(parseInt(x), parseInt(y));
+function askDifficultyUntilOk() {
+    return askDifficulty().catch((reason) => {
+        console.log(reason.message);
+        return askDifficultyUntilOk();
     });
 }
+
+function askDifficulty() {
+    return new Promise((resolve, reject) => {
+        readline.question(
+            'Select one of the following difficulty levels (beginner, intermediate, expert):',
+            (input) => {
+                if (['beginner', 'intermediate', 'expert'].includes(input))
+                    resolve(input);
+                else
+                    reject(new Error(`unknown difficulty level: ${input}`))
+            }
+        );
+    });
+}
+
+function playRound(game) {
+    askCoordinateToReveal(game)
+        .then(c => {
+            game.revealSquareAt(c);
+            if (!game.isEnded())
+                return playRound(game);
+            else {
+                printBoard(game);
+                if (game.isCleared())
+                    console.log('Congratulations, you have cleared the minefield!!');
+                else
+                    console.log('Unfortunately you were blown up!');
+                process.exit(0);
+            }
+        }).catch(err => {
+        console.log(err.message);
+        playRound(game);
+    });
+}
+
+function askCoordinateToReveal(game) {
+    printBoard(game);
+    return new Promise((resolve, reject) => {
+        readline.question(
+            `Which square to reveal (x,y)?:`,
+            (xy) => {
+                let [x, y] = xy.split(',');
+                resolve({x: parseInt(x), y: parseInt(y)});
+            });
+    });
+}
+
+function printBoard(game) {
+    console.log(game.toString());
+}
+
+playGame();
